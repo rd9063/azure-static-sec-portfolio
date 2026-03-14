3db25b82de7bc721c4c9c46b75fbd2e58fff652d
Ravi Dangi <rd9063@gmail.com>
2026-03-14 10:50:21 +0530

    Delete api directory

--- api/status/index.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: { ok: true, message: "API is running" }
-  };
-};
